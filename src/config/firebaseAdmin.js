a52fd8c4716c810649e795c93c0780f7a4207b7c
Rishi Sharma <rishi2004sh@gmail.com>
2026-01-27 14:54:01 +0530

    firebase setting

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,19 +1,10 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+}
 
-const serviceAccountPath = path.join(
-  __dirname,
-  "../../serviceAccountKey.json"
-);
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(serviceAccountPath, "utf8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
